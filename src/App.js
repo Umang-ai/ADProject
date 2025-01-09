b78c696f4c5bce13db4c3b40e81d08021104ee80
Shivam <shivamsaurav63@gmail.com>
2025-01-09 20:54:43 +0530

    added protected Routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "../src/components/ProtectedRoutes"; // Import the protected route component
 
 function App() {
   return (
@@ -10,7 +11,14 @@ function App() {
       <Routes>
         <Route path="/signIn" element={<AuthPage />} />
         <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<Dashboard />} /> 
+        <Route 
+          path="/dashboard" 
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          } 
+        />
       </Routes>
     </Router>
   );
